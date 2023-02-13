88f6ff11b1abfbb153ff6550927f2ed75d54c6ec
Daniel <38142096+adaniel2@users.noreply.github.com>
2023-02-13 15:39:23 +0100

    nvm

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,9 +7,8 @@ import javax.security.auth.login.LoginException;
 
 public class Main {
     public static void main(String[] args) throws LoginException {
-        // bot info. heroku is no longer free, hardcoding for testing
-        //JDABuilder builder = JDABuilder.createDefault(System.getenv("TOKEN"));
-        JDABuilder builder = JDABuilder.createDefault("NzcxODQwNDgxOTE0NjUwNjU2.X5x-dg.Mog5GQncx2M8xhd6-WWAVnxkrZA");
+        // bot info
+        JDABuilder builder = JDABuilder.createDefault(System.getenv("TOKEN"));
 
         // to-do: implement differently to allow mod to input these values using commands
         // CommentWatcher class should only work once these values are set "correctly"
